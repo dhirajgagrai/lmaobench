7f4645d02e21f9c10d2137a37067d00220874b1f
Dhiraj Gagrai <dhirajgagrai@gmail.com>
2025-07-16 09:55:50 +0530

    change dir structure

---
+++ src/benchmarks/disk.cpp
@@ -0,0 +1,15 @@
+#include <chrono>
+#include <fcntl.h>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std::chrono;
+
+void run_disk_benchmark(int iterations = 1000000) {
+    auto start = high_resolution_clock::now();
+
+    for (int i = 0; i < iterations; ++i) {
+    }
+
+    auto end = high_resolution_clock::now();
+}
