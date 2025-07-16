641bacddd04fcad4a004d30132c9f1d227f5e0c6
Dhiraj Gagrai <dhirajgagrai@gmail.com>
2025-07-16 23:10:25 +0530

    add disk latency

--- src/benchmarks/disk.cpp
+++ src/benchmarks/disk.cpp
@@ -1,14 +1,38 @@
 #include <chrono>
 #include <fcntl.h>
+#include <iostream>
 #include <unistd.h>
 
 using namespace std::chrono;
 
-void run_disk_benchmark(int iterations = 1000000) {
+void run_disk_benchmark(const char *disk_path) {
+    constexpr size_t BLOCK_SIZE  = 512;
+    constexpr size_t BLOCK_COUNT = 10000;
+
+    char buffer[BLOCK_SIZE];
+    int  fd = open(disk_path, O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        return;
+    }
+
     auto start = high_resolution_clock::now();
 
-    for (int i = 0; i < iterations; ++i) {
+    for (int i = 0; i < BLOCK_COUNT; ++i) {
+        off_t   offset   = i * BLOCK_SIZE;
+        ssize_t bytes_rd = pread(fd, buffer, BLOCK_SIZE, offset);
+        if (bytes_rd != BLOCK_SIZE) {
+            perror("pread");
+            close(fd);
+            return;
+        }
     }
 
     auto end = high_resolution_clock::now();
+    close(fd);
+
+    auto   total_ns = duration_cast<nanoseconds>(end - start).count();
+    double avg_ns   = static_cast<double>(total_ns) / BLOCK_COUNT;
+
+    std::cout << "[latency][disk] read 512 bytes: " << avg_ns << " ns\n";
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,27 @@
 void run_syscall_benchmark(int iterations);
 void run_signal_benchmark(int iterations);
 void run_fs_benchmark(int iterations);
+void run_disk_benchmark(const char *disk_path);
 
 int main(int argc, char **argv) {
+    const char *disk_path = nullptr;
+
     std::cout << "=== lmaobench ===\n";
     run_syscall_benchmark(1'000'000);
     run_signal_benchmark(1'000'000);
     run_fs_benchmark(1000);
+
+    for (int i = 1; i < argc; ++i) {
+        if (std::strcmp(argv[i], "-disk") == 0) {
+            disk_path = argv[i + 1];
+            if (!disk_path) {
+                std::cerr << "Usage: " << argv[0] << " -disk /dev/rdiskX\n";
+                return 1;
+            }
+
+            run_disk_benchmark(disk_path);
+        }
+    }
+
     return 0;
 }
